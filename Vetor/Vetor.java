059aac2c6d8929ada49750004f5c11820fa5c895
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-09-23 00:10:03 -0300

    Create Vetor.java

---
+++ Vetor/Vetor.java
@@ -0,0 +1,22 @@
+package vetor;
+
+
+public class Vetor {
+
+
+    public static void main(String[] args) {
+      String[] MeuVetor = new String[5]; //Declaração de um vetor do tipo String de 5 posições
+      MeuVetor[0] = "Wesley"; //Adicionando dado na posição 0 do vetor
+      MeuVetor[2] = "Petter";
+      MeuVetor[1] = "Harry";
+      MeuVetor[3] = "Nelson";
+      
+      System.out.println("Posição 1 do vetor = " + MeuVetor[0]); //Imprimindo dados de uma posição específica
+      
+      //O laço "for" abaixo imprime tudo o que tem dentro do meu vetor
+      for(int i = 0; i < MeuVetor.length; i++){
+         System.out.println("Posição " + (i + 1) + " do vetor = " + MeuVetor[i]);   
+      }
+    }
+    
+}
