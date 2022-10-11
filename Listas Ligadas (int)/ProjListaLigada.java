a8b04cd9ecd106838e3b124a87c446e671f7b40d
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-11 17:39:27 -0300

    Update ProjListaLigada.java

--- Listas Ligadas (int)/ProjListaLigada.java
+++ Listas Ligadas (int)/ProjListaLigada.java
@@ -10,21 +10,44 @@ public class ProjListaLigada {
             numeroInteiro.addLast(new Node(3));
             numeroInteiro.addLast(new Node(4));
             numeroInteiro.addFirst(new Node(5));
+            //Finção que retorna a minha lista ligada (sem inverter)
             numeroInteiro.mostraLista();
-            System.out.println("Tamanho da lista: " + numeroInteiro.size());
+            print();
             //chamada da minha função que retorna o elemento central da minha lista ligada
             elementoCentral(numeroInteiro);
+            print();
+            //chamada da minha função que inverte a minha lista ligada
+            ListaLigada listaInvertida = inverteLista(numeroInteiro);
+            listaInvertida.mostraLista();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
+        static void print(){
+        System.out.println("*********************************************************");
+    }
+    
     //Função que retorna o elemento central da minha lista ligada
     static void elementoCentral(ListaLigada lista){
-        int tamanho = lista.size();
-        int elemCentral = tamanho/2;
-        Node proxNo = lista.first();
+         int tamanho = lista.size();
+         int elemCentral = tamanho/2;
+         Node proxNo = lista.first();
         for(int i = 0; i <= elemCentral; i++)
-        proxNo = proxNo.next;
+         proxNo = proxNo.next;
         System.out.println("O elemento central é:" + proxNo.numeroInteiro);
+    }
+    
+    //Função que inverte a minha lista ligada
+    static ListaLigada inverteLista(ListaLigada lista){
+        Node ProxNo = lista.first();
+        ListaLigada listaInvertida = new ListaLigada();
+        
+        while(ProxNo != null){
+            Node no = new Node(ProxNo.numeroInteiro);
+            listaInvertida.addFirst(no);
+            ProxNo = ProxNo.next;
+        }
+        return listaInvertida;
+        
     }
 }
