3ce33108713e8756e0290073cd7c1ee148a8bf81
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-10-11 17:04:39 -0300

    Update ProjListaLigada.java

--- Listas Ligadas (int)/ProjListaLigada.java
+++ Listas Ligadas (int)/ProjListaLigada.java
@@ -10,18 +10,21 @@ public class ProjListaLigada {
             numeroInteiro.addLast(new Node(3));
             numeroInteiro.addLast(new Node(4));
             numeroInteiro.addFirst(new Node(5));
-//            numeroInteiro.addAfter(new Node(6), 7);
-//            animais.addAfter(new Node(3), 2);
-//            animais.addBefore(new Node(4), 7);
-//            animais.addBefore(new Node(4), 3);
-//            animais.addBefore(new Node(3), 2);
             numeroInteiro.mostraLista();
             System.out.println("Tamanho da lista: " + numeroInteiro.size());
-            numeroInteiro.remove(1);
-            numeroInteiro.mostraLista();
-            System.out.println("Tamanho da lista: " + numeroInteiro.size());
+            //chamada da minha função que retorna o elemento central da minha lista ligada
+            elementoCentral(numeroInteiro);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+    //Função que retorna o elemento central da minha lista ligada
+    static void elementoCentral(ListaLigada lista){
+        int tamanho = lista.size();
+        int elemCentral = tamanho/2;
+        Node proxNo = lista.first();
+        for(int i = 0; i <= elemCentral; i++)
+        proxNo = proxNo.next;
+        System.out.println("O elemento central é:" + proxNo.numeroInteiro);
     }
 }
