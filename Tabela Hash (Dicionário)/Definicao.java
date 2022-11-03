774a4307296fa5d7cf436b17a334b2c58aa57ee3
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-11-03 15:48:43 -0300

    Create Definicao.java

---
+++ Tabela Hash (Dicionário)/Definicao.java
@@ -0,0 +1,31 @@
+package tabelahashdicionario;
+
+/**
+ *
+ * @author x601533
+ */
+public class Definicao {
+    private String palavra;
+    private String traducao;
+
+    public Definicao(String palavra, String traducao) {
+        this.palavra = palavra;
+        this.traducao = traducao;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public void setPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+
+    public String getTraducao() {
+        return traducao;
+    }
+
+    public void setTraducao(String traducao) {
+        this.traducao = traducao;
+    }
+}
