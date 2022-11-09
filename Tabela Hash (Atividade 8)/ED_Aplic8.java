dd33a9788e82790841ee59aee9361bc69346f0d8
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-11-09 00:30:20 -0300

    Update ED_Aplic8.java

--- Tabela Hash (Atividade 8)/ED_Aplic8.java
+++ Tabela Hash (Atividade 8)/ED_Aplic8.java
@@ -7,3 +7,20 @@ public class ED_Aplic8{
        TreeMap<Integer, Integer> countries = new TreeMap<>();
 
        do{
+           String code, desc;
+           double preco;
+
+           i = menu();
+           switch(i){
+                case 1:
+                    System.out.println("código: ");
+                    code = s.nextLine();
+                    System.out.println("descrição: ");
+                    desc = s.nextLine();
+                    System.out.println("preço: ");
+                    preco = s.nextDouble();
+                    s.nextLine();
+                    try{
+                        
+            }                     
+           }
