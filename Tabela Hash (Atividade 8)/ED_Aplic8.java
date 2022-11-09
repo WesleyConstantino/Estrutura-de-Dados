9323d658e89f405871cd3e00533b69603fb578f8
Wesley Constantino <99278991+WesleyConstantino@users.noreply.github.com>
2022-11-09 00:19:21 -0300

    Create ED_Aplic8.java

---
+++ Tabela Hash (Atividade 8)/ED_Aplic8.java
@@ -0,0 +1,9 @@
+public class ED_Aplic8{
+
+     public static void main(String[] args){
+       int i = 0;
+       Scanner s = new Scanner(System.in);
+       TabelaHash th = new TabelaHash();
+       TreeMap<Integer, Integer> countries = new TreeMap<>();
+
+       do{
